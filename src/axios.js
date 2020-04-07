52fbce6de63887112e84d25d916edb55c3b76fcf
lourencofelipe <felipe.pucsc@gmail.com>
2020-04-07 18:18:56 -0300

    Adding interceptors

--- src/axios.js
+++ src/axios.js
@@ -3,4 +3,30 @@ import config from './config/config'
 
 axios.defaults.baseURL = config.apiURL
 
+const instance = axios.create({})
+
+instance.interceptors.request.use(config => {
+    console.log('Interceptando requisição:', config)
+    config.data = {
+        ...config.data,
+        curso: 'Vue Js'
+    }
+
+    // config.headers.common['Authorization'] =  `Bearer ${window.localStorage.getItem('token')}`
+    config.headers.common['Authorization'] =  `Bearer token_jwt`
+    config.header.put["Meu-Cabecalho"] = 'VueJS'
+
+    return config
+})
+
+instance.interceptors.response.use(response => {
+    if(Array.isArray(response.data)){
+        response.data = response.data.slice(1, 3)
+    }
+    return response
+}, error => {
+    return Promise.reject(error)
+})
+
+
 export default axios
